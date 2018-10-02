48d59eb69df4a4195014a8d0938a511a3e9a8aa2
xiaosean <xiaosean5408@gmail.com>
2018-10-02 22:36:11 +0800

    Feat: to heaps solution

--- Q23_Merge-k-Sorted-Lists.py
+++ Q23_Merge-k-Sorted-Lists.py
@@ -5,31 +5,22 @@
 #         self.next = None
 
 class Solution:
+    import heapq
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        while lists:
-            min_value = None
-            min_idxies = []
-            for idx, x in enumerate(lists):
-                if x is not None:   
-                    if min_value is None or x.val < min_value:
-                        min_idxies  = [idx]
-                        min_value = x.val
-#                 same value
-                    elif x.val == min_value:
-                        min_idxies  += [idx]
-
-            if min_value is not None:
-                first_index = min_idxies[0]
-                node = head_node = lists[first_index]
-                lists[first_index] = lists[first_index].next
-                for idx in min_idxies[1:]:
-                    node.next = lists[idx]
-                    node = node.next
-                    lists[idx] = lists[idx].next
-                node.next = self.mergeKLists(lists)
-                return head_node
-            return None                
+        heap = []
+        if lists:
+            for idx, element in enumerate(lists):
+                if element:
+                    heapq.heappush(heap, (element.val, idx, element))
+        head = cur_node = ListNode(0)
+        while heap:
+            _, idx, node = heapq.heappop(heap)
+            if node.next:
+                heapq.heappush(heap, (node.next.val, idx, node.next))
+            cur_node.next = node
+            cur_node = cur_node.next
+        return head.next
