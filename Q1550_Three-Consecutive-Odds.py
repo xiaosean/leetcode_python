66649aee58ccba86bdeaf79444d2d521b116350d
xiaosean <aa26365566@gmail.com>
2024-07-01 17:02:40 +0800

    Add:Q1550

---
+++ Q1550_Three-Consecutive-Odds.py
@@ -0,0 +1,11 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        cnt = 0
+        for num in arr:
+            if num%2:
+                cnt += 1
+                if cnt == 3:
+                    return True
+            else:
+                cnt = 0
+        return False
